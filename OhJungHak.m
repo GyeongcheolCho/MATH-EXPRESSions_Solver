30b8cc282b05b276320b18b0dc1c361aa29366e7
Gyeongcheol Cho <cho.1240@osu.edu>
2024-09-12 22:16:38 -0400

    [Update] Version 2.1 - Revised prompts and error messages for improved clarity and user experience.

--- OhJungHak.m
+++ OhJungHak.m
@@ -1,35 +1,62 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Solver for the STEAM game, MATH EXPRESSions
-% For example
-%    Rank = 33
-%    Max_equ=50;
-%    choose=1;
-% For example, [89+20-47=62]
-%     Step1:    9+6*124=753
-%              [11100121100]
-%              [1,1,1,0,0,1,2,1,1,0,0];
-%     Step2:    8-9+6+42=47
-%              [21111021201]
-%              [2,1,1,1,1,0,2,1,2,0,1];
-%     Step3:    89+29-46=72
-%              [22220221212]
-%              [2,2,2,2,0,2,2,1,2,1,2];
-%     Step4:    89+20-47=62
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MATH-EXPRESSions Solver
+% Objective:
+% This program is designed to help solve challenges in the game MATH-EXPRESSions,
+% Author: Gyeongcheol Cho
+% Last Updated: September 12, 2024
+% Developed with: MATLAB 2023b
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% How to Use the Program:
+% 1. Run the Program:
+% - Open and run the file OhJungHak.mat in MATLAB.
+% 2. Enter the Current Rank:
+% - When prompted, input the current rank (e.g., 31) and press Enter.
+% 3. Choose Equation Input Method:
+% - You will be asked if you want to input the initial equation manually.
+% - Type 'Y' to input your own equation (e.g., '2+9348=9350').
+% - Type 'N' to let the program generate equations based on further options.
+% 4. Set Duplication Limits:
+% - If you choose not to input your own equation, the program will ask how many times 
+%   numbers can be duplicated in the equation.
+% - Enter a small whole number (e.g., 2) and press Enter.
+% 5. Define the Number of Attempts:
+% - Next, specify how many equations the program should attempt to generate.
+% - For example, you might enter 10000 to ensure a comprehensive search. If no suitable 
+%   equations are found, you may need to increase this value and try again.
+% 6. Enter Feedback from the Game:
+% - Once the equation is entered or generated, input the results of the equation from the game.
+%   The color codes are as follows:
+%   r = Red (completely incorrect)
+%   l = Lime (correct number but wrong location)
+%   g = Green (correct number and correct location)
+% - For example, if the game returns results such as two reds, four greens, and two limes, 
+%   you would input 'rrggggrllgg' in the MATLAB command window.
+% 7. Repeat the Process:
+% - The program will ask if you'd like to try another equation.
+% - Repeat the process until the correct equation is found, ideally within six attempts.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Update Note:
+% Version 1 (November 10, 2022)
+% - Published the initial version.
+% Version 2 (March 11, 2023)
+% - Updated the search algorithm for the optimal equation.
+% Version 2.1 (September 12, 2024)
+% - Revised prompts and error messages for improved clarity and user experience.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Additional Notes:
+% - The program allows for flexibility in solving equations by either manually entering equations 
+%   or letting the program generate multiple equations based on your preferences.
+% - Keep in mind that larger values for attempts or duplicates might be needed if the program 
+%   struggles to find a valid equation. Adjust these as necessary.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Set-up
 % Define 
-    %Vec_Answer
-    %Best_Answer,
-    %Best_Score,
-    %Boxes_Cards,
-    %Boxes_Index,
+    %Vec_Answer,Best_Answer,Best_Score,Boxes_Cards,Boxes_Index,
     %Mat_Nrange = N_CardType by 3 mat = [current number in equ, min number, max number]
-    %loc_Equal
-    %loc_Oper
-% Vec_Answer & Best_Answer, Best_Score
+    %loc_Equal, loc_Oper
+
 clear; clc;
 Rank = input('Rank?');
-%Max_equ=50;
 
 Cards_Operator=["+","-","*","/","^"]';
 Total_N_Oper=length(Cards_Operator);
@@ -38,7 +65,6 @@ N_info = 5 + floor((Rank-1)/Total_N_Oper);
     Best_Answer=Vec_Answer;
     Best_Score=0;
     
-% Boxes, loc_Equal, loc_Oper
 Card_Equal="=";
 Cards_Number=string(0:1:9)';
 N_Oper=rem(Rank,Total_N_Oper);
@@ -56,9 +82,9 @@ N_CardType=length(Vec_Cards);
 Vec_cN_for_each_Card   = zeros(N_CardType,1);
 Vec_minN_for_each_Card   = zeros(N_CardType,1);
 %Vec_maxN_for_each_Card   = ones(N_CardType,1)*ceil(N_info/N_CardType)+4;
-flag_ini=(input('Do you want to input the initial equation by yourself? (Y/N)')=='Y');
+flag_ini=(input('Do you want to input the initial equation manually? (Y/N)')=='Y');
 if flag_ini; N_duplicate=1;
-else; N_duplicate=input('How many times do you allow numbers to be duplicated?');
+else; N_duplicate=input('How many times should numbers and operators be allowed to duplicate?');
 end
     Vec_maxN_for_each_Card   = ones(N_CardType,1)*N_duplicate;
     Mat_Nrange = [Vec_cN_for_each_Card,Vec_minN_for_each_Card,Vec_maxN_for_each_Card];
@@ -98,12 +124,12 @@ order_search=[1,order_bk_operator,[2,9,3,8,4,7,5,6,1,0]+2];
     x=1; 
     ind_search=true(1,N_info);
     if flag_ini
-        List_final_equ=input('Please insert the equation you want to try.\n');    
+        List_final_equ=input('Please enter the equation you would like to try.\n');    
         Max_equ=0;
     else
         flag_try=true;
         while flag_try            
-            Max_try=input('How many equations are you going to attempt? ');
+            Max_try=input('How many equations would you like to attempt? ');
             N_equ_gen=0;
             N_equ_try=0;
             List_equ=strings(0,N_info);
@@ -119,7 +145,7 @@ order_search=[1,order_bk_operator,[2,9,3,8,4,7,5,6,1,0]+2];
                 fprintf('\n')
                 flag_try=false;
             else
-                fprintf('\nWe could not find any valid equation given %d. Please try again after increasing the maximum number of trials\n',Max_try)
+                fprintf('\nNo valid equation was found after %d attempts. Please try again by increasing the maximum number of trials.\n',Max_try)
             end
         end
     end
@@ -130,12 +156,12 @@ order_search=[1,order_bk_operator,[2,9,3,8,4,7,5,6,1,0]+2];
 step=1;
 while step<7
     step=step+1;
-    if size(List_final_equ,1)>1; loc_choose=input('Which equation are you gonna choose? ');
+    if size(List_final_equ,1)>1; loc_choose=input('Which equation would you like to choose? (e.g., enter 1 for the 1st equation) ');
     else; loc_choose=1; end
     Vec_Answer_tp1=List_final_equ(loc_choose,:);
     flag_input_again=true;
     while flag_input_again
-        result_vec=input(sprintf(['\nEnter a character vector of the results below (r = red, l = lime, g= green) \n', ...
+        result_vec=input(sprintf(['\nEnter a character vector of the results below (r = red, l = lime, g = green) \n', ...
                           'Equation:  %s   \n',...
                           '        : '],char(List_final_equ(loc_choose,:)))); % 
         result=zeros(1,N_info);
@@ -158,13 +184,13 @@ while step<7
     end
     fprintf('\n');
         
-    flag_insert=(input('Do you want to input the equation by yourself? (Y/N) ')=='Y');
+    flag_insert=(input('Do you want to input the initial equation manually? (Y/N) ')=='Y');
     if flag_insert
-        List_final_equ=input('Insert your equation.\n   ');
+        List_final_equ=input('Please enter the equation you would like to try.\n   ');
     else
         flag_try=true;
         while flag_try
-            Max_try=input('How many equations are you going to attempt? ');
+            Max_try=input('How many equations would you like to attempt? ');
             N_equ_try=0;
             List_equ=strings(0,N_info);
             List_score=zeros(0,0);
@@ -179,7 +205,7 @@ while step<7
                 fprintf('\n')
                 flag_try=false;
             else
-                fprintf('\nWe could not find any equation given %d. Please try again aftv er increasing the maximum number of trials\n',Max_try)
+                fprintf('\nNo valid equation was found after %d attempts. Please try again by increasing the maximum number of trials.\n',Max_try)
             end
         end
     end
